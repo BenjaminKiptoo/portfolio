f528aef56a1a96247e4fffcf2b43ea38ba7fe8ae
Benkip001 <kiptoo925@gmail.com>
2022-08-19 18:14:08 +0300

    home section styling partially done

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(Document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scroll > 20){
+            $('.navbar').addClass('sticky');
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+    })
+})
